0fa4ec497254cced5ef44171829fc579c0c6204d
fahim0509 <anik.fahim07@gmail.com>
2025-01-08 12:36:07 +0600

    static ts

--- opp/access-modifier.ts
+++ opp/access-modifier.ts
@@ -1,14 +1,14 @@
-class myAcc {
-  name: string;
-  id: number;
-  protected balance: number;
-  constructor(name: string, id: number, balance: number) {
-    this.name = name;
-    this.id = id;
-    this.balance = balance;
-  }
-}
+// class myAcc {
+//   name: string;
+//   id: number;
+//   protected balance: number;
+//   constructor(name: string, id: number, balance: number) {
+//     this.name = name;
+//     this.id = id;
+//     this.balance = balance;
+//   }
+// }
 
-const myAccount = new myAcc("Fahim", 1341, 1000);
-// myAccount.balance = 3000 ---not accessible
-console.log(myAccount);
+// const myAccount = new myAcc("Fahim", 1341, 1000);
+// // myAccount.balance = 3000 ---not accessible
+// console.log(myAccount);
